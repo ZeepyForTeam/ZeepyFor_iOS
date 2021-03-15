5578cb9a436b7a7072e9b5ec635bb6280c803879
김태훈 <kimtaehoon@gimtaehcBookPro.kornet>
2021-03-15 16:08:11 +0900

    Firebase 추가, popupDialogue 추가

---
+++ Zeepy/Zeepy/Global Extentions/UIExtensions/UILable+.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Zeepy
+//
+//  Created by 김태훈 on 2021/03/15.
+//
+
+import Foundation
+import UIKit
+extension UILabel {
+
+}
--- Zeepy/Zeepy/Supports/AppDelegate.swift
+++ Zeepy/Zeepy/Supports/AppDelegate.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+  
+  var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    FirebaseApp.configure()
+    Messaging.messaging().delegate = self
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+    } else {
+      let settings: UIUserNotificationSettings =
+      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+
+    application.registerForRemoteNotifications()
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let rootVC = UIViewController()
+    window?.rootViewController = rootVC
+    window?.makeKeyAndVisible()
     return true
   }
 
@@ -78,4 +101,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
 }
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  func userNotificationCenter(
+      _ center: UNUserNotificationCenter,
+      willPresent notification: UNNotification,
+      withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+      Messaging.messaging().appDidReceiveMessage(notification.request.content.userInfo) // userinfo를 didReceiveMessage로 보내서 처리하는건지?
+      completionHandler([.alert, .sound, .badge])
+  }
+}
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
 
+    let dataDict:[String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+}
