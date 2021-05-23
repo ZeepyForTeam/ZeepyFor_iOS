4a118470ac718c5b359e3a29410b0973d929f622
JUBY <uon9816@naver.com>
2021-05-23 19:56:21 +0900

    mapView layout 완료

---
+++ Zeepy/Zeepy/Scene/Search/View/Cell/FloatingButtonCollectionViewCell.swift
@@ -0,0 +1,97 @@
+//
+
+//  FloatingButtonCollectionViewCell.swift
+
+//  Zeepy
+
+//
+
+//  Created by JUEUN KIM on 2021/05/16.
+
+//
+
+ 
+
+import UIKit
+
+ 
+
+class FloatingButtonCollectionViewCell: UICollectionViewCell {
+
+    
+
+    var circleButton = UIButton().then{
+
+        $0.isSelected = true
+
+        $0.isUserInteractionEnabled = false
+
+    }
+
+    var buttonTitle = UILabel().then{
+
+        $0.font = UIFont(name: "NanumSquareRoundOTFEB", size: 10.0)
+
+        $0.textColor = .black
+
+    }
+
+ 
+
+    func makeButton(imageName: String, buttonName: String) {
+
+        self.backgroundColor = .mainYellow
+
+        circleButton.setImage(UIImage(named: imageName), for: .normal)
+
+        buttonTitle.text = buttonName
+
+    }
+
+    
+
+    override init(frame: CGRect) {
+
+        super.init(frame: frame)
+
+        setUpCollectionViewCell()
+
+    }
+
+    
+
+    required init?(coder: NSCoder) {
+
+        fatalError("init(coder:) has not been implemented")
+
+    }
+
+    
+
+    func setUpCollectionViewCell(){
+
+        self.addSubview(circleButton)
+
+        self.addSubview(buttonTitle)
+
+        circleButton.snp.makeConstraints(){
+
+            $0.top.left.right.equalToSuperview()
+
+        }
+
+        buttonTitle.snp.makeConstraints(){
+
+            $0.top.equalTo(circleButton.snp.bottom)
+
+            $0.centerX.equalTo(circleButton)
+
+            $0.bottom.equalToSuperview()
+
+        }
+
+    }
+
+}
+
+ 
