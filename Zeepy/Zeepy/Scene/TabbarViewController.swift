0dcb7e56018993f3fb6b86ef9c372a1fd7f26568
JUBY <uon9816@naver.com>
2021-05-23 19:56:21 +0900

    mapView layout 완료

--- Zeepy/Zeepy/Scene/TabbarViewController.swift
+++ Zeepy/Zeepy/Scene/TabbarViewController.swift
@@ -35,7 +35,7 @@ extension TabbarViewContorller : UITabBarControllerDelegate {
 
 //    let tab2NavigationController =  LookAroundViewController()
     let tab2NavigationController = DetailInformationViewController()
-    let tab3NavigationController = CommunityViewController()
+    let tab3NavigationController = MapViewController()
     let tab4NavigationController = MyPageViewController()
     let vc = [tab1NavigationController, tab2NavigationController, tab3NavigationController, tab4NavigationController]
     self.setViewControllers(vc, animated: true)
