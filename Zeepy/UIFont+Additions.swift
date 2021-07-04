40d223019e3b93331b41d0ded26fb3dbef5ad395
JUBY <uon9816@naver.com>
2021-07-05 01:26:38 +0900

    로그인뷰1

---
+++ Zeepy/UIFont+Additions.swift
@@ -0,0 +1,41 @@
+//
+//  UIFont+Additions.swift
+//  zeepy_iOS
+//
+//  Generated on Zeplin. (2021. 7. 5.).
+//  Copyright (c) 2021 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+  class var textStyle7: UIFont {
+    return UIFont(name: "Gilroy-ExtraBold", size: 16.0)!
+  }
+
+  class var textStyle: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-ExtraBold", size: 16.0)!
+  }
+
+  class var textStyle6: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-Regular", size: 14.0)!
+  }
+
+  class var textStyle4: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-ExtraBold", size: 12.0)!
+  }
+
+  class var textStyle2: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-ExtraBold", size: 12.0)!
+  }
+
+  class var textStyle5: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-Regular", size: 12.0)!
+  }
+
+  class var textStyle3: UIFont {
+    return UIFont(name: "NanumSquareRoundOTF-ExtraBold", size: 10.0)!
+  }
+
+}
--- Zeepy/Zeepy/Scene/Search/View/ViewController/LoginViewController.swift
+++ Zeepy/Zeepy/Scene/Search/View/ViewController/LoginViewController.swift
@@ -13,58 +13,101 @@ class LoginViewController: UIViewController {
     let contentView = UIView().then{
         $0.backgroundColor = .white
     }
-    let loginTitle = UILabel().then{
-        $0.text = "Login"
+    let viewTitle = UIImageView().then{
+        $0.image = UIImage(named: "group926")
     }
-    let idTitle = UILabel().then{
-        $0.text = "아이디"
+    let logoImage = UIImageView().then{
+        $0.image = UIImage(named: "0408Character1")
     }
-    let idTextField = UITextField().then{
-        $0.placeholder = "아이디를 입력해주세요"
-        $0.font = UIFont(name: "NanumSquareRoundOTFR", size: 12.0)
+    let signUpLabel = UILabel().then{
+        $0.text = "Sign up"
+        $0.font = UIFont(name: "Gilroy-ExtraBold", size: 16.0)
     }
-    let passwordTitle = UILabel().then{
-        $0.text = "비밀번호"
+    let emailButton = UIButton().then{
+        $0.setImage(UIImage(named: "group920"), for: .normal)
     }
-    let passwordTextField = UITextField().then{
-        $0.placeholder = "비밀번호를 입력해주세요"
-        $0.font = UIFont(name: "NanumSquareRoundOTFR", size: 12.0)
+    let kakaoButton = UIButton().then{
+        $0.setImage(UIImage(named: "group921"), for: .normal)
     }
-    let socialLogin = UILabel().then{
-        $0.text = "소셜로그인"
+    let naverButton = UIButton().then{
+        $0.setImage(UIImage(named: "group922"), for: .normal)
     }
-    let snsOptionButton = UIButton().then{
-        $0.setImage(UIImage(named: "btnOption1"), for: .normal)
+    let alredyAccount = UIButton().then{
+        $0.setTitle("이미 계정이 있으신가요?", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NanumSquareRoundOTFEB", size: 10.0)
+        $0.setTitleColor(.mainBlue, for: .normal)
     }
-    let loginButton = UIButton().then{
-        $0.setTitle("로그인", for: .normal)
+    let findIDButton = UIButton().then{
+        $0.setTitle("아이디 찾기", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NanumSquareRoundOTFEB", size: 10.0)
+        $0.titleLabel?.textColor = .black
+        $0.setTitleColor(.grayText, for: .normal)
     }
-    let signInButton = UIButton().then{
-        $0.setTitle("회원가입", for: .normal)
+    let findPWButton = UIButton().then{
+        $0.setTitle("비밀번호 찾기", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NanumSquareRoundOTFEB", size: 10.0)
+        $0.setTitleColor(.grayText, for: .normal)
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(contentView)
+        contentView.snp.makeConstraints{
+            $0.top.bottom.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+        }
         self.view.backgroundColor = .white
         addConstraints()
     }
-
     func addConstraints(){
-        contentView.snp.makeConstraints{
-            $0.center.height.width.equalToSuperview()
+        contentView.adds([viewTitle,logoImage,signUpLabel,emailButton,kakaoButton,naverButton,alredyAccount,findIDButton,findPWButton])
+        viewTitle.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(30)
+            $0.leading.equalToSuperview().offset(25)
+            $0.height.equalTo(70)
+            $0.width.equalTo(200)
         }
-        
-        contentView.adds([loginTitle,idTitle])
-        
-        loginTitle.snp.makeConstraints{
-            $0.top.equalToSuperview().inset(30)
-            $0.leading.equalToSuperview().inset(16)
+        logoImage.snp.makeConstraints{
+            $0.top.equalTo(viewTitle.snp.bottom)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.height.equalTo(200)
+            $0.width.equalTo(200)
         }
-        idTitle.snp.makeConstraints{
-            $0.top.equalTo(loginTitle.snp.bottom).inset(10)
-            $0.leading.equalTo(loginTitle)
-            $0.bottom.equalToSuperview()
+        signUpLabel.snp.makeConstraints{
+            $0.top.equalTo(logoImage.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(40)
+            $0.height.equalTo(50)
+            
         }
-        
+        emailButton.snp.makeConstraints{
+            $0.top.equalTo(signUpLabel.snp.bottom).offset(16)
+            $0.trailing.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        kakaoButton.snp.makeConstraints{
+            $0.top.equalTo(emailButton.snp.bottom).offset(8)
+            $0.trailing.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        naverButton.snp.makeConstraints{
+            $0.top.equalTo(kakaoButton.snp.bottom).offset(8)
+            $0.trailing.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        alredyAccount.snp.makeConstraints{
+            $0.top.equalTo(naverButton.snp.bottom).offset(8)
+            $0.leading.equalTo(naverButton.snp.leading).offset(50)
+            $0.bottom.equalToSuperview().offset(-90)
+        }
+        findPWButton.snp.makeConstraints{
+            $0.top.equalTo(alredyAccount.snp.top).offset(8)
+            $0.trailing.equalTo(naverButton.snp.trailing).offset(-50)
+            $0.bottom.equalToSuperview().offset(-90)
+        }
+        findIDButton.snp.makeConstraints{
+            $0.top.equalTo(alredyAccount.snp.top).offset(8)
+            $0.trailing.equalTo(findPWButton.snp.leading).offset(-8)
+            $0.bottom.equalToSuperview().offset(-90)
+        }
     }
 }
+
