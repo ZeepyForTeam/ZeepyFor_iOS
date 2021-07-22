da1cab0d5cc649c3eafa834d950d38ca3a50e1c8
hansolnoh95 <hansolnoh95@naver.com>
2021-07-23 05:00:12 +0900

    :art: 마이페이지 메인뷰 레이아웃 완료

--- Zeepy/Zeepy/Scene/TabbarViewController.swift
+++ Zeepy/Zeepy/Scene/TabbarViewController.swift
@@ -35,7 +35,7 @@ extension TabbarViewContorller : UITabBarControllerDelegate {
     tab1NavigationController.viewControllers = [LoginViewController()]
 
     let tab2NavigationController = UINavigationController()
-    tab2NavigationController.viewControllers = [LoginEmailViewController()]
+    tab2NavigationController.viewControllers = [MypageViewController()]
     
     let tab3NavigationController = UINavigationController()
     tab3NavigationController.viewControllers = [MapViewController()]
