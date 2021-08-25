b6c401ac7385725282aa800ee2c72b1d7651deb5
hansolnoh95 <hansolnoh95@naver.com>
2021-08-25 10:43:11 +0900

    for merge

--- Zeepy/Zeepy/Scene/Login/View/Cell/InputBoxView.swift
+++ Zeepy/Zeepy/Scene/Login/View/Cell/InputBoxView.swift
@@ -10,52 +10,53 @@ import SnapKit
 import Then
 
 class InputBoxView: UIView {
-    let contentView = UIView()
-    let infoTitle = UILabel().then{
-        $0.font = UIFont(name: "NanumSquareRoundOTFB", size: 16.0)
-    }
-    let infoTextFieldBackGroundView = UIView().then{
-        $0.backgroundColor = .gray244
-        $0.setRounded(radius: 6)
-    }
-    let infoTextField = UITextField().then{
-        $0.font = UIFont(name: "NanumSquareRoundOTFB", size: 13.0)
-        $0.attributedPlaceholder = NSAttributedString(string: "placeholder text",
-                                     attributes: [NSAttributedString.Key.foregroundColor: UIColor.grayText])
-    }
+  let contentView = UIView()
+  let infoTitle = UILabel().then{
+    $0.font = .nanumRoundBold(fontSize: 12)
+  }
+  let infoTextFieldBackGroundView = UIView().then{
+    $0.backgroundColor = .gray244
+    $0.setRounded(radius: 8)
+  }
+  let infoTextField = UITextField().then{
+    $0.font = .nanumRoundBold(fontSize: 12)
+    $0.attributedPlaceholder = NSAttributedString(
+      string: "placeholder text",
+      attributes: [NSAttributedString.Key.foregroundColor: UIColor.grayText])
+  }
   let validationResult = UILabel().then {
     $0.setupLabel(text: "", color: .heartColor, font: .nanumRoundBold(fontSize: 11))
     $0.isHidden = true
   }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.addSubview(contentView)
+    contentView.snp.makeConstraints{
+      $0.top.bottom.leading.trailing.equalToSuperview()
     }
+    contentView.adds([infoTitle,infoTextFieldBackGroundView, validationResult])
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.addSubview(contentView)
-        contentView.snp.makeConstraints{
-            $0.top.bottom.leading.trailing.equalToSuperview()
-        }
-        contentView.adds([infoTitle,infoTextFieldBackGroundView, validationResult])
-        
-        infoTitle.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(10)
-        }
-        infoTextFieldBackGroundView.snp.makeConstraints{
-            $0.top.equalTo(infoTitle.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview().inset(16)
-        }
-        infoTextFieldBackGroundView.adds([infoTextField])
-        infoTextField.snp.makeConstraints{
-            $0.bottom.equalToSuperview()
-            $0.leading.equalToSuperview().offset(10)
-            $0.centerY.equalToSuperview()
-            $0.height.equalTo(50)
-        }
-      validationResult.snp.makeConstraints{
-        $0.trailing.equalTo(infoTextFieldBackGroundView)
-        $0.bottom.equalTo(infoTextField.snp.top).offset(6)
-      }
+    infoTitle.snp.makeConstraints{
+      $0.leading.equalToSuperview().offset(10)
     }
+    infoTextFieldBackGroundView.snp.makeConstraints{
+      $0.top.equalTo(infoTitle.snp.bottom).offset(8)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+    infoTextFieldBackGroundView.adds([infoTextField])
+    infoTextField.snp.makeConstraints{
+      $0.bottom.equalToSuperview()
+      $0.leading.equalToSuperview().offset(10)
+      $0.centerY.equalToSuperview()
+      $0.height.equalTo(42)
+    }
+    validationResult.snp.makeConstraints{
+      $0.trailing.equalTo(infoTextFieldBackGroundView)
+      $0.bottom.equalTo(infoTextField.snp.top).offset(6)
+    }
+  }
 }
